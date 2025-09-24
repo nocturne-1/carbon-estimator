f5d9787005083067ff3f92e69a278bc2bfb55ba5
Arianna R <arirah09@gmail.com>
2025-09-24 15:05:57 -0400

    quick comments & js edits

---
+++ app.js
@@ -0,0 +1,12 @@
+// Set dict for energy used by common appliances
+//Calculate energy used in kWh
+
+// Use GeoCode API to geocode places & get coordinates
+// Calculate distance between places with Leaflet
+
+// Send Carbon Interface API Request with parameters from Above
+
+// For Vehicle, send Vehicle Make and Model API Requests
+// Then send Carbon Interface API Request using vehicle_id
+
+//Store User Data in Firebase
