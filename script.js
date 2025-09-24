c1fb45b2c3f64b7a114fdfa8b1e9898724436fb2
Arianna R <arirah09@gmail.com>
2025-09-24 14:50:34 -0400

    made form page based on carbon interface api parameters

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOIJMRb_KCx4q_tQhgL-cFEnpNBY-QEL4",
+  authDomain: "carbon-estimator-be728.firebaseapp.com",
+  projectId: "carbon-estimator-be728",
+  storageBucket: "carbon-estimator-be728.firebasestorage.app",
+  messagingSenderId: "285316820471",
+  appId: "1:285316820471:web:8d33ad5cc4a5e6dfff52f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
